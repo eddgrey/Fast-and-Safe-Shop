64bcad2f0be7d0bcb438984d7183379096500b9c
Lau <73617885+RoseWilsonx@users.noreply.github.com>
2021-06-06 14:57:32 -0500

    TEST v1.0

--- src/components/CrearCuenta.jsx
+++ src/components/CrearCuenta.jsx
@@ -1,88 +1,92 @@
-import FormField from "./FormField";
 import Bienvenida from "./Bienvenida";
+import useForm from "./useForm";
+import validate from "./validarInfo";
 import "../styles/styles.css";
-//import { useHistory } from "react-router-dom";
-//import { ToastContainer, toast } from "react-toastify";
-//import { useRef } from "react";
-// import { tipoUsuarioContext } from "../context/TipoUsuarioContext";
 
 const CrearCuenta = () => {
-  //const nombreRef = useRef(null);
-  //const apellidoRef = useRef(null);
-  //const fechaNamcimientaRef = useRef(null);
-  //const entidadFederativaRef = useRef(null);
-  //const emailRef = useRef(null);
-  //const passwordRef = useRef(null);
-
-  //const historial = useHistory();
-  //const showError = () => toast.error("Complete los campos faltantes");
-  //const removeQue = () => toast.clearWaitingQueue();
-
-  /*const handleSumbit = (e) => {
-    e.preventDefault();
-    if (nombreRef.current.value.length === 0) {
-      console.log("error");
-      showError();
-    } else {
-      historial.push("/");
-    }
-    removeQue();
-  };*/
-
+  const { handleChange, values, handleSubmit, errors } = useForm(validate);
+  const rowForm = "w-full h-screen flex flex-row bg-blueGray-800";
+  const labelForm = "mb-1 w-1/4 text-sm uppercase tracking-wide";
+  const separate = "flex flex-row w-full mb-8 justify-between";
+  const inputForm = "input-dark pl-2  w-3/4 ";
   return (
-    <section className="w-full h-screen flex flex-row bg-blueGray-800">
-      <form className="login">
+    <section className={rowForm}>
+      <form className="login" onSubmit={handleSubmit}>
         <p className=" text-3xl mb-3">Crear Cuenta</p>
 
-        <FormField
-          id="nombre"
-          label="Nombre: "
-          type="text"
-          size="small"
-          name="nombreUsuario"
-        />
+        <div className={separate}>
+          <label className={labelForm}>Nombre:</label>
+          <input
+            className={inputForm}
+            id="nombre"
+            type="text"
+            name="nombreUsuario"
+            value={values.nombreUsuario}
+            onChange={handleChange}
+          />
+        </div>
+        {errors.nombreUsuario && <p>{errors.nombreUsuario}</p>}
+        <div className={separate}>
+          <label className={labelForm}>Apellidos:</label>
+          <input
+            className={inputForm}
+            id="apellidoUsuario"
+            type="text"
+            name="apellidoUsuario"
+            value={values.apellidoUsuario}
+            onChange={handleChange}
+          />
+        </div>
 
-        <FormField
-          id="apellidos"
-          label="Apellidos: "
-          type="text"
-          size="small"
-          name="apellidoUsuario"
-        />
+        <div className={separate}>
+          <label className={labelForm}>Fecha de Nacimiento:</label>
+          <input
+            className={inputForm}
+            id="fechaNacimiento"
+            type="text"
+            name="fechaNacimiento"
+            value={values.fechaNacimiento}
+            onChange={handleChange}
+          />
+        </div>
 
-        <FormField
-          id="fechaNacimiento"
-          label="Fecha de nacimiento: "
-          type="date"
-          size="small"
-          name="fechaNacimiento"
-        />
+        <div className={separate}>
+          <label className={labelForm}>Correo electrónico</label>
+          <input
+            className={inputForm}
+            id="email"
+            type="text"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+          />
+        </div>
 
-        <FormField
-          id="email"
-          label="Correo electrónico: "
-          type="email"
-          size="small"
-          name="email"
-        />
+        <div className={separate}>
+          <label className={labelForm}>Contraseña</label>
+          <input
+            className={inputForm}
+            id="password"
+            type="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+          />
+        </div>
 
-        <FormField
-          id="password"
-          label="Contraseña: "
-          type="password"
-          size="small"
-          name="password"
-        />
-
-        <FormField
-          id="password2"
-          label="Confirmar contraseña: "
-          type="password2"
-          size="small"
-          name="password2"
-        />
+        <div className={separate}>
+          <label className={labelForm}>Contraseña</label>
+          <input
+            className={inputForm}
+            id="password2"
+            type="password"
+            name="password2"
+            value={values.password2}
+            onChange={handleChange}
+          />
+        </div>
         <span className="otro_link">
-          ¿Ya tienes una cuenta? Inicia sesión 
+          ¿Ya tienes una cuenta? Inicia sesión
           <a className="link_login" href="/login">
             Aquí
           </a>
---
+++ src/components/useForm.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+const useForm = (validate) => {
+  const [values, setValues] = useState({
+    nombreUsuario: "",
+    apellidoUsuario: " ",
+    fechaNacimiento: " ",
+    email: " ",
+    password: " ",
+    password2: " ",
+  });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validate(values));
+  };
+
+  return { handleChange, values, handleSubmit, errors };
+};
+
+export default useForm;
